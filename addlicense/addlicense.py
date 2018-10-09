5bf325d1f195b35e497295f5b2bc035c8894f60c
Hossein Ghodse <hossein.ghodse@gmail.com>
2018-10-09 16:36:07 +0100

    moved the core script into a function

--- addlicense/addlicense.py
+++ addlicense/addlicense.py
@@ -1,5 +1,4 @@
 #!python3
-
 #     addlicense automatically inserts a specified license file or copyright message at the top of one or more source code files
 # 
 #     Copyright (C) 2018  Hossein Ghodse
@@ -21,63 +20,62 @@
 
 import argparse
 import shutil
-
-parser = argparse.ArgumentParser(description='Automatically inserts a specified license file or copyright message at ' \
-                                            'the top of one or more source code files')
-parser.add_argument('--licensefile', default="LICENSE_NOTICE.txt",
-                    help='a file containing the license or copyright text, defaulting to LICENSE.txt')
-parser.add_argument('--commentblock',
-                    help='a space-separated string indicating the characters to use at the beginning and end of the ' \
-                    'license message to demark them as a comment block')
-parser.add_argument('--comment',
-                    help='a string indicating the characters to use at the beginning of each line of the  ' \
-                    'license message to demark them as comments')
-parser.add_argument('-s','--skip-shebang-executable', action='store_true',
-                    help='skip the initial shebang executable command: if the source file starts with a comment symbol '\
-                    '(identified via the --comment option) ' \
-                    'followed by a shebang, to indicate an executable script on a POSIX system, then the license ' \
-                    'text will be inserted AFTER this initial line')
-parser.add_argument('--backup', action="store_true",
-                    help='keep a copy of the original source-file with a .bak extension')
-parser.add_argument('sourcefiles', nargs='+',
-                    help='a list of files to update with the license or copyright message')
-
-
-args = parser.parse_args()
-
-comment=''
-if(args.comment):
-    comment=args.comment + ' '
-if(args.commentblock):
-    (opencomment,closecomment)=args.commentblock.split()
-licensetext=[]
-with open(args.licensefile,'r') as f:
-    licensetext_lines=f.readlines()
-    for line in licensetext_lines:
-        line = comment + line
-        licensetext.append(line)
-    licensetext.append('\n\n')
-
-for filename in args.sourcefiles:
-
-    print('Processing {}'.format(filename))
-    if args.backup:
-        dest=filename+'.bak'
-        shutil.copyfile(filename, dest)
-
-    with open(filename,'r') as f:
-        lines = f.readlines()
-        #text = licensetext + '\n\n' + f.read()
-
-    with open(filename,'w') as f:
-        if args.skip_shebang_executable:
-            if lines[0].startswith(args.comment+'!'):
-                f.write(lines[0])
-                lines=lines[1:] # remove the first line, having now written it
-
-        f.writelines(licensetext + lines)
-
-print("\nDone!")
-
 def main():
     print()
+    parser = argparse.ArgumentParser(description='Automatically inserts a specified license file or copyright message at ' \
+                                                'the top of one or more source code files')
+    parser.add_argument('--licensefile', default="LICENSE_NOTICE.txt",
+                        help='a file containing the license or copyright text, defaulting to LICENSE.txt')
+    parser.add_argument('--commentblock',
+                        help='a space-separated string indicating the characters to use at the beginning and end of the ' \
+                        'license message to demark them as a comment block')
+    parser.add_argument('--comment',
+                        help='a string indicating the characters to use at the beginning of each line of the  ' \
+                        'license message to demark them as comments')
+    parser.add_argument('-s','--skip-shebang-executable', action='store_true',
+                        help='skip the initial shebang executable command: if the source file starts with a comment symbol '\
+                        '(identified via the --comment option) ' \
+                        'followed by a shebang, to indicate an executable script on a POSIX system, then the license ' \
+                        'text will be inserted AFTER this initial line')
+    parser.add_argument('--backup', action="store_true",
+                        help='keep a copy of the original source-file with a .bak extension')
+    parser.add_argument('sourcefiles', nargs='+',
+                        help='a list of files to update with the license or copyright message')
+
+
+    args = parser.parse_args()
+
+    comment=''
+    if(args.comment):
+        comment=args.comment + ' '
+    if(args.commentblock):
+        (opencomment,closecomment)=args.commentblock.split()
+    licensetext=[]
+    with open(args.licensefile,'r') as f:
+        licensetext_lines=f.readlines()
+        for line in licensetext_lines:
+            line = comment + line
+            licensetext.append(line)
+        licensetext.append('\n\n')
+
+    for filename in args.sourcefiles:
+
+        print('Processing {}'.format(filename))
+        if args.backup:
+            dest=filename+'.bak'
+            shutil.copyfile(filename, dest)
+
+        with open(filename,'r') as f:
+            lines = f.readlines()
+            #text = licensetext + '\n\n' + f.read()
+
+        with open(filename,'w') as f:
+            if args.skip_shebang_executable:
+                if lines[0].startswith(args.comment+'!'):
+                    f.write(lines[0])
+                    lines=lines[1:] # remove the first line, having now written it
+
+            f.writelines(licensetext + lines)
+
+    print("\nDone!")
+
